55e6817929e773441a5c1505638fc6c75008dfeb
williamjiamin <33894470+williamjiamin@users.noreply.github.com>
2023-01-09 10:19:52 +0800

    Initial Commit

---
+++ musicPlayerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  musicPlayerApp
+//
+//  Created by william on 2023/1/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
