26ea11b56b940bac526114b9c328c8aa2d333402
williamjiamin <33894470+williamjiamin@users.noreply.github.com>
2023-04-13 11:38:34 +0800

    init commit

--- musicPlayerApp/ViewController.swift
+++ musicPlayerApp/ViewController.swift
@@ -6,13 +6,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        playKeySound(keyName: sender.currentTitle!)
+//        print(sender.currentTitle)
+        
+    }
+    
+    
+    func playKeySound(keyName: String) {
+        let url = Bundle.main.url(forResource: keyName, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
+    
 
 
 }
